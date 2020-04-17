e1c38db39c2087014a5ba4349448cfa254db977e
Mo Russ <mauriceruss302@gmail.com>
2020-04-17 00:21:50 -0400

    Angular renderer and addEventListner added to video player

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {MatVideoComponent} from "mat-video/lib/video.component";
 
 @Component({
   selector: 'app-videoscroll',
@@ -6,11 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-player.component.css']
 })
 export class VideoPlayerComponent implements OnInit {
+  @ViewChild('video') matVideo: MatVideoComponent;
+  video: HTMLVideoElement;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    this.video = this.matVideo.getVideoTag();
+
+    // Use Angular renderer or addEventListener to listen for standard HTML5 video events
+
+    this.renderer.listen(this.video, 'ended', () => console.log('video ended'));
+    this.video.addEventListener('ended', () => console.log('video ended'));
   }
-
-
 }
