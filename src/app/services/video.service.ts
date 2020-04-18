82e2fbbcd63b6dd1f97fdddea18c0882e5cfe159
Giles Bradford <hockeyftw27@gmail.com>
2020-04-17 22:00:49 -0400

    uploading video works

--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
+import {HttpClient, HttpEvent, HttpParams, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 @Injectable({
@@ -8,16 +8,17 @@ import {Observable} from 'rxjs';
 export class VideoService {
   public API = 'http://localhost:8080/';
   constructor(private http: HttpClient) { }
+
   getVideoFromAWS(videoId: number): Observable<any> {
     return this.http.get(this.API + 'video/' + videoId);
   }
-  uploadVideo(video, title) {
+
+  uploadVideo(video, title): Observable<HttpEvent<any>> {
     const body = new FormData();
     body.append('file', video);
-    return this.http.post(this.API + 'video/upload?videoName=' + title, body);
+    body.append('videoName', title);
+    console.log(title);
+    const httpRequest = new HttpRequest('POST', this.API + 'video/upload', body);
+    return this.http.request(httpRequest);
   }
-
-  /*getVideo(video, title, path):{video:any, title:string, path: string} {
-    return {video, title, path};
-  }*/
 }
--- src/app/upload-engine/upload-engine.component.ts
+++ src/app/upload-engine/upload-engine.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {VideoService} from '../services/video.service';
+import {HttpEventType, HttpResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-upload-engine',
@@ -9,10 +10,12 @@ import {VideoService} from '../services/video.service';
 })
 export class UploadEngineComponent implements OnInit {
 
-  searchControl: FormControl;
-  debounce = 400;
+  selectedFiles: FileList;
+  currentFileUpload: File;
+  currentTitle: string;
+  progress: { percentage: number } = { percentage: 0 };
 
-  constructor(private videoService: VideoService ) { }
+  constructor(private uploadService: VideoService ) { }
 
   ngOnInit(): void {
   }
@@ -20,7 +23,24 @@ export class UploadEngineComponent implements OnInit {
   handle(file, videoTitle) {
     console.log(videoTitle);
     console.log({file});
-    this.videoService.uploadVideo(videoTitle, file).subscribe(console.log);
+    this.uploadService.uploadVideo(videoTitle, file.item[0]).subscribe(console.log);
+  }
+
+  selectFile(event) {
+    this.selectedFiles = event.target.files;
+  }
+
+  upload() {
+    this.progress.percentage = 0;
+    this.currentFileUpload = this.selectedFiles.item(0);
+    this.uploadService.uploadVideo(this.currentFileUpload, this.currentTitle).subscribe(event => {
+      if (event.type === HttpEventType.UploadProgress) {
+        this.progress.percentage = Math.round(100 * event.loaded / event.total);
+      } else if (event instanceof HttpResponse) {
+        console.log('File Uploaded!');
+      }
+    });
+    this.selectedFiles = undefined;
   }
 
 }
